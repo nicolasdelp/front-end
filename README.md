99468a25c70a52f0acafa1255bb5d373bf70e4d5
Nicolas Delplanque <delplanque.n@outlook.com>
2023-06-04 18:38:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ReactJS Application Template 
+### with ViteJS - Prettier - ESLint - PostCSS - TailwindCSS
+
+npm install
+
+npm run dev
+
+go to http://localhost:5173/
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "@assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-black underline">Hello world!</h1>
+      <img src={reactLogo} className="h-12 w-12" alt="React logo" />
+    </>
+  );
+}
+
+export default App;
